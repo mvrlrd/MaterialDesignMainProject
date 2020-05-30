b8eba53877cc30530d04d0a4dfd4db141c62c4a6
mvrlrd <igrbelykh@gmail.com>
2020-05-30 21:42:59 +0300

    added 3 themes

--- app/src/main/java/ru/mvrlrd/materialdesignmainproject/MainActivity.java
+++ app/src/main/java/ru/mvrlrd/materialdesignmainproject/MainActivity.java
@@ -21,12 +21,36 @@ import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+    public static final String EXTRA_THEME = "EXTRA_THEME";
+    private int themeNumber;
     private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        if(savedInstanceState != null){
+
+            themeNumber = savedInstanceState.getInt(EXTRA_THEME);
+
+            switch (themeNumber) {
+                case 0:
+                    setTheme(R.style.RedTheme);
+                    break;
+                case 1:
+                    setTheme(R.style.YellowTheme);
+                    break;
+                case 2:
+                    setTheme(R.style.GreenTheme);
+                    break;
+            }
+        }
+
+        initActivity();
+
+
+//        setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -56,12 +80,42 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void initActivity() {
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.standard:
+                themeNumber = 0;
+                break;
+            case R.id.purple:
+                themeNumber = 1;
+                break;
+            case R.id.brown:
+                themeNumber = 2;
+                break;
+
+        }
+        recreate();
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
 
         return true;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(EXTRA_THEME, themeNumber);
     }
 
     @Override
@@ -89,10 +143,5 @@ public class MainActivity extends AppCompatActivity {
 //    }
 
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        return super.onOptionsItemSelected(item);
 
-
-    }
 }
